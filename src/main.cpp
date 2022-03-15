8dea981de3e3a03555ff7dd7f0168ae182baa4bd
BenMckay6 <benjamin6mckay@gmail.com>
2022-03-15 17:06:17 -0300

    more setup info

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@
 //keep track of elasped time for power off sequence
 elapsedMillis timeElapsed;
 
+time_t getTeensy3Time() {
+  return Teensy3Clock.get();
+}
 
 
 void setup() 
@@ -27,6 +30,51 @@ digitalWrite(5, HIGH);
 // initialize interrupt input pin
  pinMode(6, INPUT_PULLUP);
  attachInterrupt(digitalPinToInterrupt(6), powerOff, LOW);
+
+// Serial INIT
+  Serial.begin(115200); // serial baud rate
+  Serial.setTimeout(2);
+
+  // I2C Bus init
+  Wire.begin();
+  Wire1.begin();
+  Wire2.begin();
+
+  Wire.setClock(400000);
+  Wire1.setClock(400000);
+  Wire2.setClock(400000);
+
+  // Display startup screen
+  setSyncProvider(getTeensy3Time);
+  display.clearDisplay();
+  display.fillRect(0,0,128,64, SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_BLACK);
+  display.setCursor(20,16);
+  display.println("FMG Controller");
+  display.setCursor(19,30);
+  display.print("Date:");
+  display.print(month());
+  display.print("-");
+  display.print(day());
+  display.print("-");
+  display.println(year());
+  display.setCursor(24,44);
+  display.print("Version 0.01");
+  display.display();
+  delay(1000);
+
+  const int t1 = threads.addThread(manageSerial, nullptr, 128); 
+  if (t1 == -1) {
+        Serial.println("error creating thread t1");
+  }
+  // set up thread for writing and reading from I2C
+
+  const int t2 = threads.addThread(manageI2C, nullptr, 128);
+  if (t2 == -1) {
+        Serial.println("error creating thread t2");
+  }
+
 
 
 }
